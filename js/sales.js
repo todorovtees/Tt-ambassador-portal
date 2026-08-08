cbdefd53cc03914ea61e601e9791fde769d12704
todorovtees <office@todorovtees.com>
2026-08-08 19:57:07 +0300

    Change currency display from лв. to € in sales table

--- js/sales.js
+++ js/sales.js
@@ -13,9 +13,9 @@ function renderRows(sales) {
     <tr>
       <td data-label="Поръчка" class="mono">#${s.order_number}</td>
       <td data-label="Дата">${new Date(s.sale_date).toLocaleDateString("bg-BG")}</td>
-      <td data-label="Стойност на поръчката" class="mono">${Number(s.sale_value).toFixed(2)} лв.</td>
+      <td data-label="Стойност на поръчката" class="mono">${Number(s.sale_value).toFixed(2)} €</td>
       <td data-label="Процент комисиона" class="mono">${s.commission_rate}%</td>
-      <td data-label="Комисиона" class="mono">${Number(s.commission_amount).toFixed(2)} лв.</td>
+      <td data-label="Комисиона" class="mono">${Number(s.commission_amount).toFixed(2)} €</td>
       <td data-label="Статус"><span class="pill pill-${s.status}">${labelFor(s.status)}</span></td>
     </tr>`).join("");
 }
